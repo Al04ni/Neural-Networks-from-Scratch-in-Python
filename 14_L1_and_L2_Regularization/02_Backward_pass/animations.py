c7a6812c2dd58aa553a41b35a79fc04e70af76f5
John Machado <iamjohnmachado@gmail.com>
2025-04-04 21:47:54 -0500

    L1 and L2 Regularization

---
+++ 14_L1_and_L2_Regularization/02_Backward_pass/animations.py
@@ -0,0 +1,10 @@
+
+#       ----- Animations -----
+# Regularization 1 - training with regularization
+# https://nnfs.io/abc/
+
+# Regularization 2 - training with regularization and more training data
+# https://nnfs.io/bcd/
+
+# Regularization 3 - training with regularization and more training data (tuned)
+# https://nnfs.io/cde/
